1e48cc2b99237b2d66b92c58ab370ea13a8020c3
krohak <rohaksinghal14@gmail.com>
2020-12-16 22:07:23 +0800

    Applied view binding

--- Destinations_Gallery_Single/app/src/main/java/com/example/destinations_gallery_single/MainActivity.kt
+++ Destinations_Gallery_Single/app/src/main/java/com/example/destinations_gallery_single/MainActivity.kt
@@ -1,26 +1,31 @@
 package com.example.destinations_gallery_single
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.destinations_gallery_single.adapter.ItemAdapter
 import com.example.destinations_gallery_single.data.Datasource
+import com.example.destinations_gallery_single.databinding.ActivityMainBinding
 import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val mActionBarToolbar: MaterialToolbar = findViewById(R.id.my_toolbar);
+        val mActionBarToolbar: MaterialToolbar = binding.myToolbar;
         setSupportActionBar(mActionBarToolbar);
         supportActionBar?.title = this.resources.getString(R.string.app_name);
 
         // Initialize data.
         val myDataset = Datasource().loadDestinations()
 
-        val recyclerView: RecyclerView = findViewById(R.id.recycler_view)
+        val recyclerView: RecyclerView = binding.recyclerView;
+
         recyclerView.adapter = ItemAdapter(this, myDataset)
 
         // Use this setting to improve performance if you know that changes
--- Destinations_Gallery_Single/app/src/main/java/com/example/destinations_gallery_single/adapter/ItemAdapter.kt
+++ Destinations_Gallery_Single/app/src/main/java/com/example/destinations_gallery_single/adapter/ItemAdapter.kt
@@ -2,43 +2,37 @@ package com.example.destinations_gallery_single.adapter
 
 import android.content.Context
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.destinations_gallery_single.R
+import com.example.destinations_gallery_single.databinding.ListItemBinding
 import com.example.destinations_gallery_single.model.Destination
-import org.w3c.dom.Text
 import java.text.NumberFormat
 
 class ItemAdapter(private val context: Context, private val dataset: List<Destination>) :
-    RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+        RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-    class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
-        val titleTextView: TextView = view.findViewById(R.id.item_title)
-        val imageView: ImageView = view.findViewById(R.id.item_image)
-        val numberView: TextView = view.findViewById(R.id.item_number)
-        val priceTextView: TextView = view.findViewById(R.id.item_price)
-        val datesTextView: TextView = view.findViewById(R.id.item_dates)
+    class ItemViewHolder(private val listItemBinding: ListItemBinding) : RecyclerView.ViewHolder(listItemBinding.root) {
+
+        fun bind(destinationItem: Destination, context: Context, position: Int) {
+            listItemBinding.itemTitle.text = context.resources.getString(destinationItem.stringResourceId)
+            listItemBinding.itemImage.setImageResource(destinationItem.imageResourceId)
+            listItemBinding.itemNumber.text = position.toString()
+            val formattedPrice: Int = context.resources.getInteger(destinationItem.priceIntResourceId)
+            listItemBinding.itemPrice.text = NumberFormat.getCurrencyInstance().format(formattedPrice)
+            listItemBinding.itemDates.text = context.resources.getString(destinationItem.datesStringResourceId)
+
+        }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        // create a new view
-        val adapterLayout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_item, parent, false)
-
-        return ItemViewHolder(adapterLayout)
+        val listItemBinding = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ItemViewHolder(listItemBinding)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val item = dataset[position]
-        holder.titleTextView.text = context.resources.getString(item.stringResourceId)
-        holder.imageView.setImageResource(item.imageResourceId)
-        holder.numberView.text = position.toString()
-        val formattedPrice: Int = context.resources.getInteger(item.priceIntResourceId)
-        holder.priceTextView.text = NumberFormat.getCurrencyInstance().format(formattedPrice)
-        holder.datesTextView.text = context.resources.getString(item.datesStringResourceId)
+        val destinationItem = dataset[position]
+        holder.bind(destinationItem, context, position)
     }
 
     override fun getItemCount() = dataset.size
